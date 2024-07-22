e0d5658098868ac3412635a488906e8ef1a278ae
Dharan Padhiyar <dpadhiya@ualberta.ca>
2024-07-22 10:35:04 -0600

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,91 +27,60 @@ const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
 
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+// // custom select variables
+// const select = document.querySelector("[data-select]");
+// const selectItems = document.querySelectorAll("[data-select-item]");
+// const selectValue = document.querySelector("[data-select-value]");
+// // const projectsFilterBtn = document.querySelectorAll(".projects [data-filter-btn]");
 
-  testimonialsItem[i].addEventListener("click", function () {
+// select.addEventListener("click", function () { elementToggleFunc(this); });
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+// // add event in all select items
+// for (let i = 0; i < selectItems.length; i++) {
+//   selectItems[i].addEventListener("click", function () {
 
-    testimonialsModalFunc();
+//     let selectedValue = this.innerText.toLowerCase();
+//     selectValue.innerText = this.innerText;
+//     elementToggleFunc(select);
+//     filterFunc(selectedValue);
 
-  });
+//   });
+// }
 
-}
+// // filter variables
+// const projecttsFilterItems = document.querySelectorAll(".projects [data-filter-item]");
+// const filterFunc = function (selectedValue) {
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+//   for (let i = 0; i < projecttsFilterItems.length; i++) {
 
+//     if (selectedValue === "all") {
+//       projecttsFilterItems[i].classList.add("active");
+//     } else if (selectedValue === projecttsFilterItems[i].dataset.category) {
+//       projecttsFilterItems[i].classList.add("active");
+//     } else {
+//       projecttsFilterItems[i].classList.remove("active");
+//     }
+//   }
+// }
 
+// // add event in all filter button items for large screen
+// let lastClickedBtn = projectsFilterBtn[0];
+// for (let i = 0; i < projectsFilterBtn.length; i++) {
 
-// custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
+//   projectsFilterBtn[i].addEventListener("click", function () {
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+//     let selectedValue = this.innerText.toLowerCase();
+//     selectValue.innerText = this.innerText;
+//     filterFunc(selectedValue);
 
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
+//     lastClickedBtn.classList.remove("active");
+//     this.classList.add("active");
+//     lastClickedBtn = this;
 
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
+//   });
 
-  });
-}
-
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
+// }
 
 
 
